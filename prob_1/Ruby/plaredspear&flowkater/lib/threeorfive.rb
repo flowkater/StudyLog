c9618098b63a4257257f9ad3e5f5692565a20c05
ChoJaewoo <chjw102@gmail.com>
2013-03-08 19:26:20 +0900

    success lee & cho

--- prob_1/Ruby/plaredspear&flowkater/lib/threeorfive.rb
+++ prob_1/Ruby/plaredspear&flowkater/lib/threeorfive.rb
@@ -1,3 +1,13 @@
 class Threeorfive
-	
+
+	def ten_less
+		(1...1000).select {|i| i % 3 == 0 || i % 5 == 0}.sort.uniq.inject(&:+)
+	end
+
+	def thousand_less
+		arr = []
+		(1..1000).each do |num|
+			arr << num if (num % 3 == 0) || (num % 5 == 0)
+		end
+	end
 end
--- prob_1/Ruby/plaredspear&flowkater/spec/threeorfive_spec.rb
+++ prob_1/Ruby/plaredspear&flowkater/spec/threeorfive_spec.rb
@@ -4,9 +4,13 @@ require 'spec_helper'
 describe "Threeorfive" do
 	let(:threeorfive){Threeorfive.new}
 
-	describe "" do
-		it "" do
-			threeorfive.method.should == ""
+	describe "less_method" do
+		it "ten_less" do
+			threeorfive.ten_less.should == 233168
+		end
+
+		it "thousand_less" do
+			# threeorfive.thousand_less.should == 1000
 		end
 	end
 end
