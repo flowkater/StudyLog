efe3eeb24e43be898bb0c2bdae713b9e9adf208e
ChoJaewoo <chjw102@gmail.com>
2013-03-08 18:56:35 +0900

    basic folder structure

---
+++ prob_1/Ruby/plaredspear&flowkater/spec/threeorfive_spec.rb
@@ -0,0 +1,12 @@
+require 'threeorfive'
+require 'spec_helper'
+
+describe "Threeorfive" do
+	let(:threeorfive){Threeorfive.new}
+
+	describe "" do
+		it "" do
+			threeorfive.method.should == ""
+		end
+	end
+end
