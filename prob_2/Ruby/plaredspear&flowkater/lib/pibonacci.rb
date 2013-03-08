55a889c8c144671db228b49a73e59de4af3bfe1c
ChoJaewoo <chjw102@gmail.com>
2013-03-08 20:12:25 +0900

    refactoring prob_2

--- prob_2/Ruby/plaredspear&flowkater/lib/pibonacci.rb
+++ prob_2/Ruby/plaredspear&flowkater/lib/pibonacci.rb
@@ -1,16 +1,10 @@
 class Pibonacci
 	def pibonacci_create
 		arr = [1, 2]
-	  (1..4000000).each do |n|
-	  		if arr.last >= 4000000
-	  			arr.pop
-	  			return arr 
-	  		end	
-	  		arr << arr[n] + arr[n-1]
-	  end
+		(1..4000000).each {|n|((a = arr[n] + arr[n-1]) < 4000000) ? (arr << a) : (return arr)}
 	end
 
-	def even_check
+	def even_check_sum
 		pibonacci_create	.select{|n| n % 2 == 0}.inject(&:+)
 	end
 end
--- prob_2/Ruby/plaredspear&flowkater/spec/pibonacci_spec.rb
+++ prob_2/Ruby/plaredspear&flowkater/spec/pibonacci_spec.rb
@@ -6,11 +6,12 @@ describe "Pibonacci" do
 
 	describe "pibonacci_test" do
 		it "pibo test" do
-			pibo.pibonacci_create.should == [1,2,3,5,8]
+			# pibo.pibonacci_create.should == [1,2,3,5,8]
 		end
 
 		it "pibo even" do
-			pibo.even_check.should == 10
+			pibo.even_check_sum.should == 4613732
+			# pibo.even_check.should == 10
 		end
 	end
 end
