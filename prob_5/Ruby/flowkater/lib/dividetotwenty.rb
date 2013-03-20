8917077055f1970bcd54c9a493378c338e883f6f
ChoJaewoo <chjw102@gmail.com>
2013-03-20 21:13:15 +0900

    prob_5 solve flowkater

--- prob_5/Ruby/flowkater/lib/dividetotwenty.rb
+++ prob_5/Ruby/flowkater/lib/dividetotwenty.rb
@@ -1,18 +1,35 @@
 class Dividetotwenty
-	# Float::INFINITY -> +1.0/0.0
-	def prime_arr(n)
-		arr = []
-		(1..n).each do |num|
-			if is_prime?(num)
-				arr << num
-			end
-		end
-		
-		arr
 
+	# 최대공약수
+	def gcd(a,b)
+		return a if b == 0
+		c = a % b
+		gcd(b, c)
 	end
 
-	def divide_to_ten(n)
+	# 최소공배수
+	def lcm(a,b)
+		(a * b) / gcd(a,b)
+	end
+
+	def arr_lcm(n)
+		arr = []
+		(1..n).each{|x| arr << x}
+		arr.inject(1) {|lc,n| lcm(lc, n)}
+	end
+
+	# Float::INFINITY -> +1.0/0.0ksms
+	# def prime_arr(n)
+	# 	arr = []
+	# 	(1..n).each do |num|
+	# 		if is_prime?(num)
+	# 			arr << num
+	# 		end
+	# 	end
+	# 	arr
+	# end
+
+	# def divide_to_ten(n)
 		# arr = prime_arr(n)
 		# arr.each do |x| 
 		# 	unless n % x == 0
@@ -20,25 +37,20 @@ class Dividetotwenty
 		# 	end
 		# end
 		# true
-		(1..Float::INFINITY).each do |num|
-			result = []
-			(1..n).each do |x|
-				result << num % x
-			end
-			return num if result.select{|r| r == 0}.length == n
-		end
-	end
+		# (1..Float::INFINITY).each do |num|
+		# 	result = []
+		# 	(1..n).each do |x|
+		# 		result << num % x
+		# 	end
+		# 	return num if result.select{|r| r == 0}.length == n
+		# end
+	# end
 
-	def is_prime?(n)
-
-		if (n==1) then return false end
-
-		(2..Math.sqrt(n)).each do |num|
-			if (n % num) == 0 then return false end
-		end
-
-		true
-	end
-
-
+	# def is_prime?(n)
+	# 	if (n==1) then return false end
+	# 	(2..Math.sqrt(n)).each do |num|
+	# 		if (n % num) == 0 then return false end
+	# 	end
+	# 	true
+	# end
 end
--- prob_5/Ruby/flowkater/spec/dividetotwenty_spec.rb
+++ prob_5/Ruby/flowkater/spec/dividetotwenty_spec.rb
@@ -4,42 +4,69 @@ require 'dividetotwenty'
 describe "Dividetotwenty" do
 	let(:divide) {Dividetotwenty.new}
 
-	describe "prime_arr" do
-		it "get 2520" do
-			# divide.divide_to_ten.should == 2520
+	describe "gcd" do
+		it "between two" do
+			divide.gcd(2,4).should == 2
+			divide.gcd(2,6).should == 2
+			divide.gcd(3,6).should == 3
+			divide.gcd(3,10).should == 1
+			divide.gcd(64,100).should == 4
 		end
 
-		it "1toN" do
-			divide.prime_arr(3).should == [2,3]
-			divide.prime_arr(10).should == [2,3,5,7]
-			divide.prime_arr(20).should == [2, 3, 5, 7, 11, 13, 17, 19]
-		end
-	end
-
-	describe "divide_to_ten" do
-		it "divide_by_prime" do
-			divide.divide_to_ten(3).should == 6
-			divide.divide_to_ten(10).should == 2520
-			# divide.divide_to_ten(20).should == 2520
+		it "swap" do
+			# a,b = b,a
+			# divide.gcd(2,4).should == [4,2]
 		end
 	end
 
-
-	describe "is_prime?" do
-
-		it "true" do
-			divide.is_prime?(2).should == true
-			divide.is_prime?(3).should == true
-			divide.is_prime?(5).should == true
-			divide.is_prime?(7).should == true
+	describe "lcm" do
+		it "between two" do
+			divide.lcm(2,4).should == 4
+			divide.lcm(3,6).should == 6
 		end
 
-		it "false" do
-			divide.is_prime?(4).should == false
-			divide.is_prime?(6).should == false
+		it "arr_lcm" do
+			# divide.arr_lcm(3).should == [1,2,3]
+			divide.arr_lcm(3).should == 6
+			divide.arr_lcm(10).should == 2520
+			divide.arr_lcm(20).should == 232792560
 		end
-
 	end
 
+	# describe "prime_arr" do
+	# 	it "get 2520" do
+	# 		# divide.divide_to_ten.should == 2520
+	# 	end
 
+	# 	it "1toN" do
+	# 		divide.prime_arr(3).should == [2,3]
+	# 		divide.prime_arr(10).should == [2,3,5,7]
+	# 		divide.prime_arr(20).should == [2, 3, 5, 7, 11, 13, 17, 19]
+	# 	end
+	# end
+
+	# describe "divide_to_ten" do
+	# 	it "divide_by_prime" do
+	# 		divide.divide_to_ten(3).should == 6
+	# 		divide.divide_to_ten(10).should == 2520
+	# 		# divide.divide_to_ten(20).should == 2520
+	# 	end
+	# end
+
+
+	# describe "is_prime?" do
+
+	# 	it "true" do
+	# 		divide.is_prime?(2).should == true
+	# 		divide.is_prime?(3).should == true
+	# 		divide.is_prime?(5).should == true
+	# 		divide.is_prime?(7).should == true
+	# 	end
+
+	# 	it "false" do
+	# 		divide.is_prime?(4).should == false
+	# 		divide.is_prime?(6).should == false
+	# 	end
+
+	# end
 end
