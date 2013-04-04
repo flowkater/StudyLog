b76a74c53a249704d2203c70f960d0f41092b117
bandinside <plaredspear@gmail.com>
2013-04-04 14:30:25 +0900

    prob_8 : plaredspear

--- prob_8/Ruby/plaredspear&jh/lib/thousand.rb
+++ prob_8/Ruby/plaredspear&jh/lib/thousand.rb
@@ -1,5 +1,41 @@
 class Thousand
 
 
+  # def multiple(n)
+  #   length = n.to_s.length
+  #   ans = 1
+
+  #   (1..length).each do |num|
+  #     ans *= (n / 10**(num-1)) % 10
+  #   end
+  #   ans
+  # end
+
+
+
+
+  # def quest(n)
+  #   length = n.to_s.length - 5
+  #   ans = 0
+  #   (1..length).each do |num|
+  #     temp = multiple((n / 10**(num-1)) % 100000)
+  #     ans = temp if ans < temp
+  #   end
+
+  #   ans
+
+  # end
+
+
+
+  def multiple(n)
+    (1..5).inject(1) {|ans, num| ans *= n % 10; n /= 10; ans }
+  end
+
+  def quest(n)
+    length = n.to_s.length-5
+    (1..length).inject(0) { |ans, num| ans = (multiple(n % 100000)>ans ? multiple(n % 100000) : ans); n /= 10; ans }
+  end
+
 
 end
---
+++ prob_9/Ruby/plaredspear/spec/pythagoras_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'pythagoras'
+
+describe "pythagoras" do
+  let(:pytha) { Pythagoras.new }
+
+  
+end
