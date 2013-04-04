c346d8723ef5181a9c3368a82f4eee109cddb1f3
ChoJaewoo <chjw102@gmail.com>
2013-04-04 11:30:25 +0900

    prob_9 solve

---
+++ prob_9/Ruby/flowkater/lib/pitagoras.rb
@@ -0,0 +1,55 @@
+class Pitagoras
+	# 피타고라스의 일반해?
+	# α=u**2−v**2,β=2uv,γ=u**2+v**2
+	# 안씀 //
+
+	def check(a,b,c)
+		a**2 + b ** 2 == c ** 2
+	end
+
+	# def sum(a,b,c)
+		# a + b + c
+	# end
+	
+	def sum_check(a,b,c)
+		a + b + c if check(a,b,c)
+	end
+
+
+	def sum_check_arr(v)
+		(1..20).each do |a|
+			(1..20).each do |b|
+				if a < b
+					c = Math.sqrt(a ** 2 + b ** 2)
+					if x = divisor(v, sum_check(a,b,c))
+						return [a*x ,b*x, c*x] 
+					end
+				end
+			end
+		end
+	end
+
+	# 40은 1000의 약수
+	# [8,15,17]
+	# 40k = 1000; k = 25
+	# [8*k, 15*k, 17*k ]
+	# def divisor_test
+	# 	x = divisor(1000, sum_check(8,15,17))
+	# 	[8*x, 15*x, 17*x]
+	# end
+
+	def multiply(v) 
+		sum_check_arr(v).inject(&:*)
+	end
+
+	def divisor(n, x)
+		return false if x == nil
+		return n / x if n % x == 0
+		false
+	end
+
+	# def reverse_sum(v)
+		
+	# end
+end
+
--- prob_9/Ruby/flowkater/spec/pitagoras_spec.rb
+++ prob_9/Ruby/flowkater/spec/pitagoras_spec.rb
@@ -4,9 +4,42 @@ require 'spec_helper'
 describe "Pitagoras" do
 	let(:pita){Pitagoras.new}
 
-	describe "test" do
-		it "test" do
+	describe "pitagoras_" do
+		it "check" do
+			pita.check(3,4,5).should == true
+		end
 
+		# it "sum" do
+			# pita.sum(3,4,5).should == 12
+		# end
+
+		it "sum_check" do
+			pita.sum_check(3,4,5).should == 12
+			pita.sum_check(1,2,2).should == nil
+		end
+
+		# it "divisor_test" do
+			# pita.divisor_test.should == [200,375,425]
+		# end
+
+		it "sum_check_arr" do
+			# pita.sum_check_arr.should == [12, 30, 24, 56, 40, 36, 90, 60, 132, 48, 84, 182, 112, 80, 144, 72, 180, 70, 120, 168, 96, 150, 108, 126, 224, 160, 154, 210, 198, 156, 208, 140, 200, 176, 192, 204, 216, 228, 240, 234]
+			# pita.sum_check_arr(12).should == [3,4,5]
+			# pita.sum_check_arr(234).should == [65,72,97]
+			# pita.sum_check_arr(1000).should == [200,375,425]
+			pita.sum_check_arr(1000).should == [200.0, 375.0, 425.0]
+		end
+
+		it "multiply" do
+			pita.multiply(1000).should == 31875000
+		end
+
+		it "divisor" do
+			pita.divisor(1000,40).should == 25
+		end
+
+		it	"reverse_sum" do
+			# pita.reverse_sum(12).should == [3,4,5]
 		end
 	end
 end
